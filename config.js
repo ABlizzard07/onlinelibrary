07603899be139547ff4efe8db2c5cee009a19b1f
UniversalOverlord123 <68557624+UniversalOverlord123@users.noreply.github.com>
2021-01-24 14:05:04 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlfvwSwTE5knvxZrc8G2EKHhyYaKCVYy8",
+    authDomain: "wireleibrary-99bea.firebaseapp.com",
+    databaseURL: "https://wireleibrary-99bea.firebaseio.com",
+    projectId: "wireleibrary-99bea",
+    storageBucket: "wireleibrary-99bea.appspot.com",
+    messagingSenderId: "896930010173",
+    appId: "1:896930010173:web:adaa802144a015ae448654"
+  };
+
+export default firebaseConfig;
